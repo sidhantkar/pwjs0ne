06ea9279d7d0fc0b85f534a088fad0051d7099a0
sidhantkar <150805323+sidhantkar@users.noreply.github.com>
2023-11-22 14:21:31 +0000

    disucss operators

---
+++ basicsone/operators.js
@@ -0,0 +1,24 @@
+// Arithmatic operators
+// 5+5
+// 10-5
+// 5*5
+// 10/10
+2**3=>2*2*2
+10%3
+let valueone=3
+valueone++
+
+
+const numberone="2"
+const numbertwo="4"
+const numberthree=numberone+numbertwo
+console.log(number three);
+
+// Assignment operators
+let valuetwo=3
+valueTwo+=10
+
+// Comparision Operator
+let pw=3
+let pwskills=3
+console.log(pw==skills)
