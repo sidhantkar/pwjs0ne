e17d82d26aa9c33d432fef979100dfcda5b42a9d
sidhantkar <150805323+sidhantkar@users.noreply.github.com>
2023-11-22 05:25:06 +0000

    discussion about datatyps

---
+++ basicsone/data-type.js
@@ -0,0 +1,28 @@
+// primitive(STACK)
+/*
+String(Ex- const firstname= "Sidhant")
+Number (Ex1- const age=20)(Ex2- const score=200)
+Boolean(Ex- let islogedin=false)
+Null(Ex- const nodeverson=null)
+Undefined(Ex- let bankbalance=undefined)
+Symbol(Ex- const buttonid=symbol("id"))
+Bigint(Ex- const aReallylongnumber=9999990000555544n)
+*/
+// reference(non-primitive)-objects(HEAP)
+/*
+Arrys
+objects
+functions
+*/
+*/this is for array*/
+const numbers=[1,2,3,4,5]
+const heroes=["sidhant","kalia","litu","babul"]
+*/this is for objects*/
+cons userobjects={name: "sidhant",age:20,islogedin:true}
+*/this is for function*/
+const sayhello=function(){console.log("sidhant")}
+const registerAuser=function(){}
+const Loginuser=function(){}
+
+
+
