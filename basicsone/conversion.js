1455014a87decb1b4918dba28ef2eb0c0f7c8929
sidhantkar <150805323+sidhantkar@users.noreply.github.com>
2023-11-24 04:27:49 +0000

    discussion about conversion

---
+++ basicsone/conversion.js
@@ -0,0 +1,10 @@
+let bankbalance ='100'
+let bankbalanceint = Boolean(bankbalance);
+
+
+
+console.log( bankbalance);
+console.log( bankbalanceint);
+console.log(NaN==NaN)
+console.log( 5+ undefined);
+console.log( 5+ null);
