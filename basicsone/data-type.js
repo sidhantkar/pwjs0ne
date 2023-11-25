66fc52f6853ba717b4fea3ef5f5d9467cadc433e
sidhantkar <150805323+sidhantkar@users.noreply.github.com>
2023-11-25 05:05:15 +0000

    string discussion

--- basicsone/data-type.js
+++ basicsone/data-type.js
@@ -18,7 +18,7 @@ functions
 const numbers=[1,2,3,4,5]
 const heroes=["sidhant","kalia","litu","babul"]
 */this is for objects*/
-cons userobjects={name: "sidhant",age:20,islogedin:true}
+const userobjects={name: "sidhant",age:20,islogedin:true}
 */this is for function*/
 const sayhello=function(){console.log("sidhant")}
 const registerAuser=function(){}
---
+++ basicsone/numAndstring.js
@@ -0,0 +1,17 @@
+ const firstname = 'Hello'
+ const lastname = "hc"
+ console.log(firstname +''+ lastname);
+ console.log(`My firstname  ${firstname}  is and My last name is ${lastname}`);
+
+const username = new String ("sidhant")
+console.log(username[0]);
+console.log(username.length);
+console.log(username.charAt(0));
+console.log(username.substring(0, 4));
+
+let email = "h@hc.com   "
+console.log(email.trim());
+let emailtwo = "hc@pw.live"
+console.log(emailtwo.replace('hc','hitesh'));
+console.log(email.includes('hc'));
+console.log(emailtwo.toUpperCase());
