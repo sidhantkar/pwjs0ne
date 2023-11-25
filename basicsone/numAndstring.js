f0d0b420b53b76e0505578801f8feecfaf5a40ff
sidhantkar <150805323+sidhantkar@users.noreply.github.com>
2023-11-25 06:07:39 +0000

    disucssion about number

--- basicsone/numAndstring.js
+++ basicsone/numAndstring.js
@@ -1,4 +1,5 @@
- const firstname = 'Hello'
+/* This is discussion about STRING */
+/* const firstname = 'Hello'
  const lastname = "hc"
  console.log(firstname +''+ lastname);
  console.log(`My firstname  ${firstname}  is and My last name is ${lastname}`);
@@ -14,4 +15,17 @@ console.log(email.trim());
 let emailtwo = "hc@pw.live"
 console.log(emailtwo.replace('hc','hitesh'));
 console.log(email.includes('hc'));
-console.log(emailtwo.toUpperCase());
+console.log(emailtwo.toUpperCase()); */
+
+
+/* This is discussion about Number */
+const num= new Number (5.23456)
+console.log(num.toFixed(2));
+const subscribers =100000
+console.log(subscribers.toLocaleString());
+
+console.log(Number.MAX_VALUE);
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10));
+
