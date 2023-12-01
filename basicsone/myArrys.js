884c3cda60663a6ebb825724eb3b3b0aa363c0d0
sidhantkar <150805323+sidhantkar@users.noreply.github.com>
2023-12-01 13:35:39 +0000

    discussion about Array

---
+++ basicsone/myArrys.js
@@ -0,0 +1,13 @@
+const myArray = [1,2,3,4,5]
+const myNewArray= new Array (1,2,3,4,5)
+console.log(myArray[1]);
+const dcHeros= ["batsman","superman","flash"]
+// dcHeros.push("wonder woman")
+//dcHeros[dcHeros.length]= "wonder woman" linkedIn ପୋଷ୍ଟ ରେ ଭାଇରାଲ ହୋଇଥିଲା (SIdhant କର )
+/*console.log(dcHeros.length);*/
+// console.log(dcHeros);
+//const lastHero = dcHeros.pop()
+console.log(dcHeros.slice(1,3));
+console.log(dcHeros)
+console.log(dcHeros.splice(1,3));
+console.log(dcHeros)
